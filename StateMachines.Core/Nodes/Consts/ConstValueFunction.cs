0eaac5a76a383c5ed91a2ac641cc9dbf7c219385
idursun <ibrahim.dursun@gmail.com>
2014-05-11 12:56:48 +0300

    Add GreaterThanFunction

---
+++ StateMachines.Core/Nodes/Consts/ConstValueFunction.cs
@@ -0,0 +1,20 @@
+﻿namespace StateMachines.Core.Nodes.Consts
+{
+    public class ConstValueFunction<T> : WorkflowFunction
+    {
+        private readonly T m_value;
+
+        public ConstValueFunction(T value)
+        {
+            m_value = value;
+        }
+
+        [Output]
+        public T Value { get; set; }
+
+        public override void Evaluate()
+        {
+            Value = m_value;
+        }
+    }
+}
