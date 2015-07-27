da163b084bea3995b6bd09ebfa44efb4ea52cadd
idursun <ibrahim.dursun@gmail.com>
2015-07-27 17:08:09 +0300

    make WorkflowBuilder fluent

---
+++ src/EventMachine.Core/IWorkflowBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace EventMachine.Core
+{
+    public interface IWorkflowBuilder
+    {
+        IWorkflowBuilder Connect(Pin pin1, Pin pin2);
+        IWorkflowBuilder Connect(Pin pin1, IExecutable executable);
+        IWorkflowExecutionContext Compile();
+    }
+}
